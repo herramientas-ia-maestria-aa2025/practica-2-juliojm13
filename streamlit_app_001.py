4fb114847d26443600573924caab11a7182c0def
Julio Jimenez <juliojm1310@gmail.com>
2025-05-16 20:25:00 -0500

    Practica 2 - Julio Jimenez

--- streamlit_app_001.py
+++ streamlit_app_001.py
@@ -3,12 +3,12 @@ import mlflow.sklearn
 import numpy as np
 
 st.title("Predicción de Diabetes")
+st.markdown("**Autor: Julio Jiménez**")
 
 # Configurar conexión con MLflow Tracking Server
-pass
-
+mlflow.set_tracking_uri("http://localhost:9090") # cambiar en función de su servidor
 # Cargar modelo desde el Model Registry, revise el ejemplo de flask
-pass
+model = mlflow.sklearn.load_model("models:/01/9")
 
 # Deslizadores para cada input del modelo
 pregnancies = st.slider("Número de embarazos", 0, 20, 1)
